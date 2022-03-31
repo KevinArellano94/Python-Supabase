ee9cb9387950a759af99a1644e3f1b87d6ec983d
Kevin Arellano <Kevin.Arellano94@gmail.com>
2022-03-30 22:02:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ dotenv.load_dotenv('variable.env')
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
+# https://supabase.com/docs/guides/database/functions
 data = supabase.table("table_name").select("*").execute()
+# data = supabase.table("table_name").select("*").eq('plate_number', '123 ABC').execute()
 assert len(data.data) > 0
 
 print(data)
